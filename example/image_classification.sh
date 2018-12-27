592ad573343778a619f984b73808c27f3161a59d
zhouxin25 <zhouxin25@meituan.com>
2018-12-27 21:40:36 +0800

    update

--- example/image_classification.sh
+++ example/image_classification.sh
@@ -1,5 +1,5 @@
 python2.7 image_classification.py \
  --images=data/im_list_for_classification.txt \
  --top_k_pred=5 \
- --model_dir=checkpoints/resnet.ckpt \
+ --model_dir=checkpoints/ckpt-resnet101-mlimages-imagenet/resnet.ckpt \
  --dictionary=data/imagenet2012_dictionary.txt
